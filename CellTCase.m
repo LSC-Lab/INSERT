d33ea2ce83e69f9efa739bb0d0de4547721ddd01
h-ehsan <60522094+h-ehsan@users.noreply.github.com>
2021-02-10 20:54:45 -0500

    Copyright added

--- CellTCase.m
+++ CellTCase.m
@@ -1,5 +1,6 @@
 function MPcase = CellTCase(prop, MF, instance, feature)
 % Number of missing samples
+% Copyright 2021 Roozbeh Razavi-Far
 
 if MF < 1
     error('NUmber of missing features must greater than one')
